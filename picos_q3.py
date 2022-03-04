c4ab21a0ff280735ced3f37525532cf8fbc74168
katie608 <katief650@gmail.com>
2022-03-03 22:11:31 -0500

    Still does not work

--- picos_q3.py
+++ picos_q3.py
@@ -52,86 +52,24 @@ z35 = RealVariable('z35')
 z36 = RealVariable('z36')
 
 
-
-
-# Constraint 0: x + 2y <= 14.
-# p.add_constraint(x11 + 2 * y11 <= 14.0)
-# TODO 30% stuff
-n = x11 + x13 + x14 + x15 + x16 + x21 + x22 + x23 + x24 + x26 + x31 + x32 + x33 + x35 + x36 + y11 + y13 + y14 + y15 + y16 + y21 + y22 + y23 + y24 + y26 + y31 + y32 + y33 + y35 + y36 + z11 + z13 + z14 + z15 + z16 + z21 + z22 + z23 + z24 + z26 + z31 + z32 + z33 + z35 + z36
-
-# denominators
-h1_denom = x11 + x13 + x14 + x15 + x16 + y11 + y13 + y14 + y15 + y16 + z11 + z13 + z14 + z15 + z16 # hub 1
-h2_denom = x21 + x22 + x23 + x24 + x26 + y21 + y22 + y23 + y24 + y26 + z21 + z22 + z23 + z24 + z26 # hub 2
-h3_denom = x31 + x32 + x33 + x35 + x36 + y31 + y32 + y33 + y35 + y36 + z31 + z32 + z33 + z35 + z36 # hub 3
-
-# x numerators
-x_h1_num = x11 + x13 + x14 + x15 + x16 # hub 1
-x_h2_num = x21 + x22 + x23 + x24 + x26 # hub 2
-x_h3_num = x31 + x32 + x33 + x35 + x36 # hub 3
-
-# y numerators
-y_h1_num = y11 + y13 + y14 + y15 + y16 # hub 1
-y_h2_num = y21 + y22 + y23 + y24 + y26 # hub 2
-y_h3_num = y31 + y32 + y33 + y35 + y36 # hub 3
-
-# z numerators
-z_h1_num = z11 + z13 + z14 + z15 + z16 # hub 1
-z_h2_num = z21 + z22 + z23 + z24 + z26 # hub 2
-z_h3_num = z31 + z32 + z33 + z35 + z36 # hub 3
-
-# # add x constraints
-# p.add_constraint(0.3 * (h1_denom) <= x_h1_num <= 0.36 * (h1_denom)) # hub 1
-# p.add_constraint(0.3 * (h2_denom) <= x_h2_num <= 0.36 * (h2_denom)) # hub 2
-# p.add_constraint(0.3 * (h3_denom) <= x_h3_num <= 0.36 * (h3_denom)) # hub 3
-
-# # add y constraints
-# p.add_constraint(0.3 * (h1_denom) <= y_h1_num <= 0.36 * (h1_denom)) # hub 1
-# p.add_constraint(0.3 * (h2_denom) <= y_h2_num <= 0.36 * (h2_denom)) # hub 2
-# p.add_constraint(0.3 * (h3_denom) <= y_h3_num <= 0.36 * (h3_denom)) # hub 3
-
-# # add z constraints
-# p.add_constraint(0.3 * (h1_denom) <= z_h1_num <= 0.36 * (h1_denom)) # hub 1
-# p.add_constraint(0.3 * (h2_denom) <= z_h2_num <= 0.36 * (h2_denom)) # hub 2
-# p.add_constraint(0.3 * (h3_denom) <= z_h3_num <= 0.36 * (h3_denom)) # hub 3
-
-# # add x constraints
-# p.add_constraint(0.3 <= x_h1_num <= 0.36) # hub 1
-# p.add_constraint(0.3 <= x_h2_num <= 0.36) # hub 2
-# p.add_constraint(0.3  <= x_h3_num <= 0.36) # hub 3
-
-# # add y constraints
-# p.add_constraint(0.3 <= y_h1_num <= 0.36) # hub 1
-# p.add_constraint(0.3 <= y_h2_num <= 0.36) # hub 2
-# p.add_constraint(0.3<= y_h3_num <= 0.36) # hub 3
-
-# # add z constraints
-# p.add_constraint(0.3 <= z_h1_num <= 0.36) # hub 1
-# p.add_constraint(0.3 <= z_h2_num <= 0.36 ) # hub 2
-# p.add_constraint(0.3 <= z_h3_num <= 0.36) # hub 3
-
-# add x constraints
-p.add_constraint(0.3 <= x_h1_num) # hub 1
-p.add_constraint(0.3 <= x_h2_num) # hub 2
-p.add_constraint(0.3  <= x_h3_num) # hub 3
-p.add_constraint(x_h1_num <= 0.36) # hub 1
-p.add_constraint(x_h2_num <= 0.36) # hub 2
-p.add_constraint(x_h3_num <= 0.36) # hub 3
-
-# add y constraints
-p.add_constraint(0.3 <= y_h1_num) # hub 1
-p.add_constraint(0.3 <= y_h2_num) # hub 2
-p.add_constraint(0.3<= y_h3_num) # hub 3
-p.add_constraint(y_h1_num <= 0.36) # hub 1
-p.add_constraint(y_h2_num <= 0.36) # hub 2
-p.add_constraint(y_h3_num <= 0.36) # hub 3
-
-# add z constraints
-p.add_constraint(0.3 <= z_h1_num) # hub 1
-p.add_constraint(0.3 <= z_h2_num) # hub 2
-p.add_constraint(0.3 <= z_h3_num) # hub 3
-p.add_constraint(z_h1_num <= 0.36) # hub 1
-p.add_constraint(z_h2_num <= 0.36 ) # hub 2
-p.add_constraint(z_h3_num <= 0.36) # hub 3
+p.add_constraint(64*x11 + 0.64*x13 + 0.64*x14 + 0.64*x15 + 0.36*x16 - 0.36*y11 - 0.36*y13-0.36*y14 - 0.36*y15 - 0.36*y16 - 0.36*z11 - 0.36*z13 - 0.36*z14 - 0.36*z15 - 0.36*z16 <= 0)
+p.add_constraint(0.7*x11 + 0.7*x13 + 0.7*x14 + 0.7*x15 + 0.7*x16 - 0.3*y11 - 0.3*y13-0.3*y14 - 0.3*y15 - 0.3*y16 - 0.3*z11 - 0.3*z13 - 0.3*z14 - 0.3*z15 - 0.3*z16 >= 0)
+p.add_constraint(0.64*x21 + 0.64*x22 + 0.64*x23 + 0.64*x24 + 0.36*x26 - 0.36*y21 - 0.36*y22 - 0.36*y23-0.36*y24 - 0.36*y26 - 0.36*z21 - 0.36*z22 - 0.36*z23 - 0.36*z24 - 0.36*z26 <= 0)
+p.add_constraint(0.7*x21 + 0.7*x22 + 0.7*x23 + 0.7*x24 + 0.7*x26 - 0.3*y21 - 0.3*y22 - 0.3*y23-0.3*y24 - 0.3*y26 - 0.3*z21 - 0.3*z22 - 0.3*z23 - 0.3*z24 - 0.3*z26 >= 0)
+p.add_constraint(0.64*x31 + 0.64*x32 + 0.64*x33 + 0.64*x35 + 0.64*x36 - 0.36*y31 - 0.36*y32 - 0.36*y33-0.36*y35 - 0.36*y36 - 0.36*z31 - 0.36*z32 - 0.36*z33 - 0.36*z35 - 0.36*z36 <= 0)
+p.add_constraint(0.7*x31 + 0.7*x32 + 0.7*x33 + 0.7*x35 + 0.7*x36 - 0.3*y31 - 0.3*y32 - 0.3*y33-0.3*y35 - 0.3*y36 - 0.3*z31 - 0.3*z32 - 0.3*z33 - 0.3*z35 - 0.3*z36 >= 0)
+p.add_constraint(0.64*y11 + 0.64*y13 + 0.64*y14 + 0.64*y15 + 0.64*y16 - 0.36*x11 - 0.36*x13-0.36*x14 - 0.36*x15 - 0.36*x16 - 0.36*z11 - 0.36*z13 - 0.36*z14 - 0.36*z15 - 0.36*z16 <= 0)
+p.add_constraint(0.7*y11 + 0.7*y13 + 0.7*y14 + 0.7*y15 + 0.7*y16 - 0.3*x11 - 0.3*x13-0.3*x14 - 0.3*x15 - 0.3*x16 - 0.3*z11 - 0.3*z13 - 0.3*z14 - 0.3*z15 - 0.3*z16 >= 0)
+p.add_constraint(0.64*y21 + 0.64*y22 + 0.64*y23 + 0.64*y24 + 0.64*y26 - 0.36*x21 - 0.36*x22 - 0.36*x23-0.36*x24 - 0.36*x26 - 0.36*z21 - 0.36*z22 - 0.36*z23 - 0.36*z24 - 0.36*z26 <= 0)
+p.add_constraint(0.7*y21 + 0.7*y22 + 0.7*y23 + 0.7*y24 + 0.7*y26 - 0.3*x21 - 0.3*x22 - 0.3*x23-0.3*x24 - 0.3*x26 - 0.3*z21 - 0.3*z22 - 0.3*z23 - 0.3*z24 - 0.3*z26 >= 0)
+p.add_constraint(0.64*y31 + 0.64*y32 + 0.64*y33 + 0.64*y35 + 0.64*y36 - 0.36*x31 - 0.36*x32 - 0.36*x33-0.36*x35 - 0.36*x36 - 0.36*z31 - 0.36*z32 - 0.36*z33 - 0.36*z35 - 0.36*z36 <= 0)
+p.add_constraint(0.7*y31 + 0.7*y32 + 0.7*y33 + 0.7*y35 + 0.7*y36 - 0.3*x31 - 0.3*x32 - 0.3*x33-0.3*x35 - 0.3*x36 - 0.3*z31 - 0.3*z32 - 0.3*z33 - 0.3*z35 - 0.3*z36 >= 0)
+p.add_constraint(0.64*z11 + 0.64*z13 + 0.64*z14 + 0.64*z15 + 0.64*z16 - 0.36*x11 - 0.36*x13-0.36*x14 - 0.36*x15 - 0.36*x16 - 0.36*y11 - 0.36*y13 - 0.36*y14 - 0.36*y15 - 0.36*y16 <= 0)
+p.add_constraint(0.7*z11 + 0.7*z13 + 0.7*z14 + 0.7*z15 + 0.7*z16 - 0.3*x11 - 0.3*x13-0.3*x14 - 0.3*x15 - 0.3*x16 - 0.3*y11 - 0.3*y13 - 0.3*y14 - 0.3*y15 - 0.3*y16 >= 0)
+p.add_constraint(0.64*z21 + 0.64*z22 + 0.64*z23 + 0.64*z24 + 0.64*z26 - 0.36*x21 - 0.36*x22 - 0.36*x23-0.36*x24 - 0.36*x26 - 0.36*y21 - 0.36*y22 - 0.36*y23 - 0.36*y24 - 0.36*y26 <= 0)
+p.add_constraint(0.7*z21 + 0.7*z22 + 0.7*z23 + 0.7*z24 + 0.7*z26 - 0.3*x21 - 0.3*x22 - 0.3*x23-0.3*x24 - 0.3*x26 - 0.3*y21 - 0.3*y22 - 0.3*y23 - 0.3*y24 - 0.3*y26 >= 00.)
+p.add_constraint(0.64*z31 + 0.64*z32 + 0.64*z33 + 0.64*z35 + 0.64*z36 - 0.36*x31 - 0.36*x32 - 0.36*x33-0.36*x35 - 0.36*x36 - 0.36*y31 - 0.36*y32 - 0.36*y33 - 0.36*y35 - 0.36*y36 <= 0)
+p.add_constraint(0.7*z31 + 0.7*z32 + 0.7*z33 + 0.7*z35 + 0.7*z36 - 0.3*x31 - 0.3*x32 - 0.3*x33-0.3*x35 - 0.3*x36 - 0.3*y31 - 0.3*y32 - 0.3*y33 - 0.3*y35 - 0.3*y36 >= 0)
 
 
 # all goods to hubs
@@ -161,23 +99,17 @@ p.add_constraint(z16 + z26 + z36 == 171)
 
 
 # final set of constraints
-p.add_constraint(x13 + x23 + x33 + x14 + x24 + x15 + x35 + x16 + x26 + x36 == 977)
-p.add_constraint(y13 + y23 + y33 + y14 + y24 + y15 + y35 + y16 + y26 + y36 == 951)
-p.add_constraint(z13 + z23 + z33 + z14 + z24 + z15 + z35 + z16 + z26 + z36 == 972)
-
+p.add_constraint(x11 + x13 + x14 + x15 + x16 + y11 + y13 + y14 + y15 + y16 + z11 + z13 + z14 + z15 + z16 <= 977)
+p.add_constraint(x21 + x22 + x23 + x24 + x26 + y21 + y22 + y23 + y24 + y26 + z21 + z22 + z23 + z24 + z26 <= 951)
+p.add_constraint(x31 + x32 + x33 + x35 + x36 + y31 + y32 + y33 + y35 + y36 + z31 + z32 + z33 + z35 + z36 <= 972)
 
 
 # Objective function: 3x + 4y.
 p.set_objective('min',
-                300 * x11 + 600 * x13 + 200 * x14 + 0 * x15 + 500 * x16 +
-                0 * x21 + 400 * x22 + 300 * x23 + 500 * x24 + 300 * x26 +
-                700 * x31 + 500 * x32 + 200 * x33 + 400 * x35 + 0 * x36 +
-                300 * y11 + 600 * y13 + 200 * y14 + 0 * y15 + 500 * y16 +
-                0 * y21 + 400 * y22 + 300 * y23 + 500 * y24 + 300 * y26 +
-                700 * y31 + 500 * y32 + 200 * y33 + 400 * y35 + 0 * y36 +
-                300 * z11 + 600 * z13 + 200 * z14 + 0 * z15 + 500 * z16 +
-                0 * z21 + 400 * z22 + 300 * z23 + 500 * z24 + 300 * z26 +
-                700 * z31 + 500 * z32 + 200 * z33 + 400 * z35 + 0 * z36)
+                300*x11 + 300*y11 + 300*z11 + 600*x13 + 600*y13 + 600*z13 + 200*x14 + 200*y14 + 200*z14 +
+                500*x16 + 500*y16 + 500*z16 + 400*x22 + 400*y22 + 400*z22 + 300*x23 + 300*y23 + 300*z23 +
+                500*x24 + 500*y24 + 500*z24 + 300*x26 + 300*y26 + 300*z26 + 700*x31 + 700*y31 + 700*z31 +
+                500*x32 + 500*y32 + 500*z32 + 200*x33 + 200*y33 + 200*z33 + 400*x35 + 400*y35 + 400*z35)
 
 
 solution = p.solve(verbosity=0, solver='cvxopt', primals=False)
